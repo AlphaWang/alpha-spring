3092bcc5a976cb10da1a82bc4b3ddb037b6d8589
Alpha <awang@coupang.com>
2018-10-10 23:47:24 +0800

    [spring-cloud] init.

---
+++ alpha-spring-cloud/src/main/java/com/alphawang/spring/alphaspringcloud/AlphaSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package com.alphawang.spring.alphaspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class AlphaSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaSpringCloudApplication.class, args);
+    }
+}
