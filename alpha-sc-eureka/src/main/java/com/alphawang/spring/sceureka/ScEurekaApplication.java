8e09e505f465175165873b5a6f8613e8c06a5974
Alpha <awang@coupang.com>
2018-10-12 22:31:40 +0800

    [sc] init eureka

---
+++ alpha-sc-eureka/src/main/java/com/alphawang/spring/sceureka/ScEurekaApplication.java
@@ -0,0 +1,11 @@
+package com.alphawang.spring.sceureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class ScEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScEurekaApplication.class, args);
+    }
+}
