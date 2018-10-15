a97a9ee9ae4c8c03c17f4b25aef513abc3a1d2f8
Alpha <awang@coupang.com>
2018-10-15 23:48:06 +0800

    [spring] fall back for exceptions.

--- alpha-hystrix/src/main/java/com/alphawang/spring/alphahystrix/fallback/HystrixFallbackController.java
+++ alpha-hystrix/src/main/java/com/alphawang/spring/alphahystrix/fallback/HystrixFallbackController.java
@@ -1,17 +1,16 @@
 package com.alphawang.spring.alphahystrix.fallback;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HystrixFallbackController {
     
-//    @HystrixCommand(commandProperties = {
-//        // see HystrixCommandProperties
-//        @HystrixProperty()
-//    })
+    @HystrixCommand(fallbackMethod = "fallback")
     @GetMapping("/hystrix/fallback")
     public String testFallback() {
+        int i = 1 / 0;
         return "success";
     }
     
