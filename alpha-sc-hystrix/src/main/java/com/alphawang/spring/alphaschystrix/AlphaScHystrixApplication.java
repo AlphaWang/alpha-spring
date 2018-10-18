fcd24e703fee9d7547cbdfa47cb65d28e4baf090
Alpha <awang@coupang.com>
2018-10-18 23:12:04 +0800

    [hystrix] dashboard

--- alpha-sc-hystrix/src/main/java/com/alphawang/spring/alphaschystrix/AlphaScHystrixApplication.java
+++ alpha-sc-hystrix/src/main/java/com/alphawang/spring/alphaschystrix/AlphaScHystrixApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
 @EnableCircuitBreaker
 //@SpringCloudApplication
+@EnableHystrixDashboard  // http://localhost:8081/hystrix
 public class AlphaScHystrixApplication {
 
     public static void main(String[] args) {
