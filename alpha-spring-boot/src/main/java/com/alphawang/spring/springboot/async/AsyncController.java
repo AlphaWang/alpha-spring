625d23a495e284f133a05e2d3a0e7a0eaea52ab3
Alpha <awang@coupang.com>
2018-11-25 00:31:21 +0800

    [spring mvc] AsyncRestTemplate

---
+++ alpha-spring-boot/src/main/java/com/alphawang/spring/springboot/async/AsyncController.java
@@ -0,0 +1,36 @@
+package com.alphawang.spring.springboot.async;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@RestController
+public class AsyncController {
+    
+    @RequestMapping("/api/sync")
+    public String api() {
+        log.error(">>>>>> /api/sync");
+        
+        return "sync api result";
+    } 
+    
+    @RequestMapping("/api/async")
+    public Callable<String> asyncApi() {
+        log.error(">>>>>> /api/async");
+        
+        return new Callable<String>() {
+            @Override 
+            public String call() throws Exception {
+                log.error(" ----- Start Async...");
+                TimeUnit.SECONDS.sleep(10);
+                log.error(" ----- End Async...");
+                
+                return "async api result";
+            }
+        };
+    }
+}
---
+++ alpha-spring-boot/src/main/java/com/alphawang/spring/springboot/async/AsyncControllerTest.java
@@ -0,0 +1,32 @@
+package com.alphawang.spring.springboot.async;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.client.AsyncRestTemplate;
+
+@Slf4j
+public class AsyncControllerTest {
+
+    public static void main(String[] args) {
+        AsyncRestTemplate asyncRestTemplate = new AsyncRestTemplate();
+
+        ListenableFuture<ResponseEntity<String>> future = 
+            asyncRestTemplate.getForEntity("http://localhost:8080/api/async", String.class);
+   
+        future.addCallback(new ListenableFutureCallback<ResponseEntity<String>>() {
+            @Override 
+            public void onFailure(Throwable throwable) {
+                log.error(" ==== async response error", throwable);
+            }
+
+            @Override 
+            public void onSuccess(ResponseEntity<String> stringResponseEntity) {
+                log.error(" ==== async api success: {}", stringResponseEntity.getBody());
+            }
+        });
+        
+        log.error(" <<<< Future END.");
+    }
+}
