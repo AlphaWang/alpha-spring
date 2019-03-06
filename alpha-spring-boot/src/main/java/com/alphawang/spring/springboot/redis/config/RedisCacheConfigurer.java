1731e213b4c36c79bebb508c36c066c38d0fb1f6
Alpha <awang@coupang.com>
2019-03-06 18:21:23 +0800

    [spring] spring boot redis cache //error handler -> inner class

--- alpha-spring-boot/src/main/java/com/alphawang/spring/springboot/redis/config/RedisCacheConfigurer.java
+++ alpha-spring-boot/src/main/java/com/alphawang/spring/springboot/redis/config/RedisCacheConfigurer.java
@@ -1,16 +1,41 @@
 package com.alphawang.spring.springboot.redis.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.Nullable;
 
 @Configuration
-//@EnableCaching
+@EnableCaching
 public class RedisCacheConfigurer extends CachingConfigurerSupport {
-    @Nullable
+    
+    @Override
     public CacheErrorHandler errorHandler() {
         return new MyCacheErrorHandler();
     }
-    
+
+    @Slf4j
+    static class MyCacheErrorHandler implements CacheErrorHandler {
+        @Override
+        public void handleCacheGetError(RuntimeException e, Cache cache, Object o) {
+            log.error("==== Failed to get cache {}, {}", cache.getName(), o, e);
+        }
+
+        @Override
+        public void handleCachePutError(RuntimeException e, Cache cache, Object o, Object o1) {
+            log.error("==== Failed to put cache {}, {}", cache.getName(), o, e);
+        }
+
+        @Override
+        public void handleCacheEvictError(RuntimeException e, Cache cache, Object o) {
+            log.error("==== Failed to evict cache {}, {}", cache, o, e);
+        }
+
+        @Override
+        public void handleCacheClearError(RuntimeException e, Cache cache) {
+            log.error("==== Failed to clear cache {}", cache, e);
+        }
+    }
 }
