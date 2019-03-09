64cd8d59d147782db69c58e7fa74fb1bb5164ef2
Alpha <awang@coupang.com>
2019-03-09 23:40:27 +0800

    [redis] KeyGenerator & config

--- alpha-spring-mvc-annotation/src/main/java/com/alphawang/spring/mvc/redis/config/RedisCacheConfigurer.java
+++ alpha-spring-mvc-annotation/src/main/java/com/alphawang/spring/mvc/redis/config/RedisCacheConfigurer.java
@@ -1,6 +1,6 @@
 package com.alphawang.spring.mvc.redis.config;
 
-import com.alphawang.spring.mvc.User;
+import com.google.common.base.Joiner;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
@@ -15,6 +15,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.lang.reflect.Method;
@@ -28,12 +29,7 @@ public class RedisCacheConfigurer extends CachingConfigurerSupport {
 
     @Override 
     public KeyGenerator keyGenerator() {
-        return new KeyGenerator() {
-
-            @Override public Object generate(Object o, Method method, Object... objects) {
-                return method.getName();
-            }
-        };
+        return new MethodKeyGenerator();
     }
 
     @Override
@@ -52,12 +48,63 @@ public class RedisCacheConfigurer extends CachingConfigurerSupport {
         return redisConnectionFactory;
     }
 
+//    @Configuration
+//    public class RedisConfig extends CachingConfigurerSupport {
+//        @Autowired
+//        private RedisConnectionFactory redisConnectionFactory;
+//        @Bean
+//        public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//            Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//            objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//            serializer.setObjectMapper(objectMapper);
+//            RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//            redisTemplate.setConnectionFactory(redisConnectionFactory);
+//            redisTemplate.setKeySerializer(new StringRedisSerializer());
+//            redisTemplate.setValueSerializer(serializer);
+//            redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//            redisTemplate.setHashValueSerializer(serializer);
+//            redisTemplate.afterPropertiesSet();
+//            return redisTemplate;
+//        }
+    
+//        @Bean
+//        public RedisCacheManager redisCacheManager(RedisTemplate redisTemplate) {
+//            RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisTemplate.getConnectionFactory());
+//            RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()));
+//            return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+//        }
+        /**
+         * 二者选其一即可
+         */
+
+    //    @Bean
+    //    public RedisCacheConfiguration redisCacheConfiguration() {
+    //        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+    //        ObjectMapper objectMapper = new ObjectMapper();
+    //        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+    //        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+    //        serializer.setObjectMapper(objectMapper);
+    //        return RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+    //    }
+
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory cf) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
         redisTemplate.setConnectionFactory(cf);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(User.class)); //TODO not flexible
+        /**
+         * Object.class:
+         * java.util.LinkedHashMap cannot be cast to com.alphawang.spring.mvc.User
+         * 
+         * User.class:
+         * no exception, but not flexible.
+         */
+//        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class)); 
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());    //need upgrade version
         return redisTemplate;
     }
 
@@ -85,6 +132,25 @@ public class RedisCacheConfigurer extends CachingConfigurerSupport {
 //            .prefixKeysWith("redis-c-")
 //            .entryTtl(Duration.ofMinutes(10));
 //    }
+
+    /**
+     * Generates cache key by className + methodName + params
+     */
+    static class MethodKeyGenerator implements KeyGenerator {
+
+        private static final Joiner JOINER = Joiner.on(":").useForNull("null");
+        private static final Joiner PARAM_JOINER = Joiner.on("-").useForNull("null");
+
+        @Override
+        public Object generate(Object target, Method method, Object... params) {
+            String paramsString = "";
+            if (params != null) {
+                paramsString = PARAM_JOINER.join(params);
+            }
+
+            return JOINER.join(target.getClass().getSimpleName(), method.getName(), paramsString);
+        }
+    }
 
     @Slf4j
     static class MyCacheErrorHandler implements CacheErrorHandler {
