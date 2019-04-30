ee3d96e15d9f014ab2d16802b7167ff35f6c4f49
Alpha <awang@coupang.com>
2019-04-30 18:45:35 +0800

    [spring cloud] nacos consumer

--- alpha-sc-nacos-consumer/src/main/java/com/alphawang/spring/nacosconsumer/NacosConsumerApplication.java
+++ alpha-sc-nacos-consumer/src/main/java/com/alphawang/spring/nacosconsumer/NacosConsumerApplication.java
@@ -1,13 +1,41 @@
 package com.alphawang.spring.nacosconsumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NacosConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NacosConsumerApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
+    @RestController
+    class TestController {
+        
+        @Autowired
+        private RestTemplate restTemplate;
+        
+        @GetMapping("/echo/{str}")
+        public String echo(@PathVariable String str) {
+            String response = restTemplate.getForObject("http://nacos-service-provider/echo/" + str, String.class);
+            return "Nacos Client Wrapper: " + response;
+        }
+        
+    }
 }
