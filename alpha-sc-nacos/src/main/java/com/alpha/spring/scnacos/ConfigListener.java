41c2fb3668b0cfa7b3e73e4f147e111b04fc7836
AlphaWang <1919wang@gmail.com>
2019-05-01 21:53:53 +0800

    [nacos] test config listener. (failed)

--- alpha-sc-nacos/src/main/java/com/alpha/spring/scnacos/ConfigListener.java
+++ alpha-sc-nacos/src/main/java/com/alpha/spring/scnacos/ConfigListener.java
@@ -3,21 +3,18 @@ package com.alpha.spring.scnacos;
 import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.config.annotation.NacosConfigListener;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
-public class ScNacosApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ScNacosApplication.class, args);
-    }
-
+@Configuration
+public class ConfigListener {
+    
     @NacosInjected
     private ConfigService configService;
-
+    
+    //TODO 为什么不生效？
     @NacosConfigListener(dataId = "cart_front_api.properties")
     public void onMessage(String config) {
-        System.out.println("NACOS UPDATED 2 " + config);
+        System.out.println("NACOS UPDATED " + config);
     }
 }
